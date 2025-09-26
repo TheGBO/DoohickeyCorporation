5bea2dd8bb43b1244b60ec5090bda3fceae6c737
NullCyan <gabrielthejota@gmail.com>
2025-09-26 13:07:58 -0300

    fibonatiated

--- Fibonacio/fibonacio.c
+++ Fibonacio/fibonacio.c
@@ -1,24 +1,37 @@
+//$ gcc fibonacio.c -o fibonacio.elf
+//$ ./fibonacio.elf (count) (print everything)
+//$ ./fibonacio.elf 10 true
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2){
-        printf("usage: ./fibonacio.elf <count>\n");
-        return 1;
-    }
+	if(argc < 3){
+		printf("usage: ./fibonacio.elf <count> <print everything>\n");
+		return 1;
+	}
 
-    unsigned int numberCount = atoi(argv[1]);
+	unsigned int numberCount = atoi(argv[1]);
 
-    unsigned long long a = 1;
-    unsigned long long b = 1;
-    for (int i = 0; i < numberCount; i++)
-    {
-    printf("%llu \n", a);
-        unsigned long long c = a + b;
-        a = b;
-        b = c;
-    }
+	long long a = 1;
+	long long b = 1;
+	bool printEverything = strcmp(argv[2], "true") == 0;
+	for (int i = 0; i < numberCount; i++)
+	{
+		long long c = a + b;
+		a = b;
+		b = c;
+		if(printEverything)
+		{
+			printf("%lld \n", a);
+		}
+	}
+	if(!printEverything)
+	{
+		printf("%lld \n", a);
+	}
 
     return 0;
 }
