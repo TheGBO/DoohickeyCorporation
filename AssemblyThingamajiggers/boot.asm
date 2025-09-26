9058e4b526b60f41dca7059cdda77a1031817abb
NullCyan <gabrielthejota@gmail.com>
2025-09-26 14:58:48 -0300

    yes

--- AssemblyThingamajiggers/boot.asm
+++ AssemblyThingamajiggers/boot.asm
@@ -1,32 +1,45 @@
 BITS 16
-;ORG 0x7C00
-global clearScreen
-global start
-global printX
+ORG 0x7C00
 
 start:
-    call clearScreen
-    call printX
-.halt:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    
+    ; Setup stack
+    mov ss, ax
+    mov sp, 0x7C00
+    
+    ; Load kernel from disk to 0x1000
+    mov ah, 0x02        ; BIOS read sector function
+    mov al, 4           ; Number of sectors to read (adjust as needed)
+    mov ch, 0           ; Cylinder number
+    mov cl, 2           ; Sector number (sectors start at 1, not 0!)
+    mov dh, 0           ; Head number
+    mov dl, 0x80        ; Drive number (0x80 for first hard disk)
+    mov bx, 0x1000      ; ES:BX = destination buffer (0x1000:0x0000)
+    mov es, bx
+    mov bx, 0x0000
+    int 0x13            ; BIOS disk interrupt
+    jc disk_error       ; Jump if error (carry flag set)
+    
+    ; Jump to kernel
+    jmp 0x1000:0x0000
+
+disk_error:
+    mov si, error_msg
+    mov ah, 0x0E
+.print_char:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .print_char
+.done:
     hlt
-    jmp .halt
+    jmp .done
 
-; Clear screen (scroll up entire window with attribute)
-clearScreen:
-    pusha
-    mov ah, 0x00
-    mov al, 0x03
-    int 0x10
-    popa
-    ret
-
-printX:
-    pusha
-    mov ah, 0x0e
-    mov al, 'X'
-    int 0x10
-    popa
-    ret
+error_msg db "Disk error!", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
